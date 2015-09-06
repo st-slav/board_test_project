12bc08a4b50f7a4b882d71b086ef3502e2f3395a
st-slav <st.mam_@hotmail.com>
2015-09-06 15:15:14 +0500

    add arhetip

---
+++ src/test/java/board/RegistryTest.java
@@ -0,0 +1,25 @@
+package board;
+
+import org.openqa.selenium.support.PageFactory;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import board.pages.HomePage;
+
+public class RegistryTest extends TestBase {
+
+  private HomePage homepage;
+
+  @BeforeMethod
+  public void initPageObjects() {
+    homepage = PageFactory.initElements(driver, HomePage.class);
+  }
+
+  @Test
+  public void testHomePageHasAHeader() {
+    driver.get(baseUrl);
+    Assert.assertFalse("".equals(homepage.header.getText()));
+  }
+}
